be8043298fb30b73bccac3aa30189cc92c287288
Lizi Boros <boroslizi@gmail.com>
2019-05-28 14:55:36 +0200

    ShoppingCartServlet logic started

--- src/main/java/com/codecool/shop/controller/ShoppingCartServlet.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartServlet.java
@@ -1,17 +1,31 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.order.LineItem;
+import com.codecool.shop.order.Order;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/shopping-cart"})
 public class ShoppingCartServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Order order = Order.getInstance();
+        List<LineItem> items = order.getItems();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("items", items);
+        engine.process("product/shopping_cart.html", context, resp.getWriter());
 
     }
 
