a8f9e10be5b6f1a5a350edecb67328d399b822cc
Petrav <vagi.p94@gmail.com>
2019-05-29 11:34:42 +0200

    new classes for the shopping cart

--- src/main/java/com/codecool/shop/order/Order.java
+++ src/main/java/com/codecool/shop/order/Order.java
@@ -1,12 +1,15 @@
 package com.codecool.shop.order;
 
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private List<LineItem> items = new ArrayList<>();
+    private List<LineItem> itemList = new ArrayList<>();
     private static Order instance = null;
-
 
     private Order() {}
 
@@ -18,7 +21,52 @@ public class Order {
     }
 
     public List<LineItem> getItems() {
-        return items;
+        return itemList;
+    }
+
+
+
+    public void add(int id){
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        List<Product> items = productDataStore.getAll();
+
+        for (Product item:items) {
+
+            // getting the proper Product (by id)
+            if (item.getId() == id) {
+                boolean itemIsAlreadyInCart = false;
+
+                // if the product is already in the cart, it increases the quantity of the lineitem
+                for (LineItem orderItem: itemList) {
+                    if (orderItem.getProduct().equals(item)) {
+                        orderItem.setQuantity(orderItem.getQuantity() + 1);
+                        itemIsAlreadyInCart = true;
+                    }
+                    break;
+                }
+
+                // if the product isn't in the cart, it adds the lineitem to it
+                if (!itemIsAlreadyInCart) {
+                    LineItem lineItem = new LineItem(item);
+                    itemList.add(lineItem);
+                }
+            }
+            break;
+        }
+    }
+
+    public void remove(int id){
+        for (LineItem lineItem:itemList) {
+            if(lineItem.getProduct().getId()==id){
+                if (lineItem.getQuantity() > 1) {
+                    lineItem.setQuantity(lineItem.getQuantity() - 1);
+                } else {
+                    itemList.remove(lineItem);
+                }
+                break;
+            }
+        }
     }
 
 }
