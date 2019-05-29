4e09da51296dfc7be4feb9fa8fa835ebcb50b8f1
Petrav <vagi.p94@gmail.com>
2019-05-29 14:57:47 +0200

    Initial files for checkout

---
+++ src/main/java/com/codecool/shop/order/User.java
@@ -0,0 +1,9 @@
+package com.codecool.shop.order;
+
+public class User {
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private String billingAddress;
+    private String shippingAddress;
+}
