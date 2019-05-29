90919ba83291979f591af98b73824cd3fd6de631
Petrav <vagi.p94@gmail.com>
2019-05-29 20:33:40 +0200

    User.java: new attributes for address

--- src/main/java/com/codecool/shop/order/User.java
+++ src/main/java/com/codecool/shop/order/User.java
@@ -4,6 +4,12 @@ public class User {
     private String name;
     private String email;
     private int phoneNumber;
+    private String billingCountry;
+    private String billingCity;
+    private int billingZip;
     private String billingAddress;
+    private String shippinCountry;
+    private String shipingCity;
+    private int shipingZip;
     private String shippingAddress;
 }
