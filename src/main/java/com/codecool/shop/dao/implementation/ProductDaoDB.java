a05dc51ec7af5a040139d051e4088ccbd97c9581
Lizi Boros <boroslizi@gmail.com>
2019-06-11 11:28:12 +0200

    ProductDaoDB initialized

---
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoDB.java
@@ -0,0 +1,42 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.util.List;
+
+public class ProductDaoDB extends DB_connection implements ProductDao {
+
+    @Override
+    public void add(Product product) {
+
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+
+}
