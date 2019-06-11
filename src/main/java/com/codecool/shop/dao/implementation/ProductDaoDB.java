c55ea7c1455a54dbe2a251c03bf62e2bd6481dbe
Lizi Boros <boroslizi@gmail.com>
2019-06-11 11:36:04 +0200

    Add and remove implemented

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoDB.java
@@ -9,9 +9,26 @@ import java.util.List;
 
 public class ProductDaoDB extends DB_connection implements ProductDao {
 
+    private static ProductDaoDB instance = null;
+
+    public static ProductDaoDB getInstance() {
+        if (instance == null) {
+            instance = new ProductDaoDB();
+        }
+        return instance;
+    }
+
     @Override
     public void add(Product product) {
-
+        String statement = "INSERT INTO categories (id, name, description, defaultprice, defaultcurrency, category_id, supplier_id) " +
+                "VALUES (DEFAULT, '" + product.getName() +
+                "', '" + product.getDescription() +
+                "', '" + product.getDefaultPrice() +
+                "', '" + product.getDefaultCurrency() +
+                "', '" + product.getProductCategory().getId() +
+                "', '" + product.getSupplier().getId() +
+                "');";
+        super.executeStatement(statement);
     }
 
     @Override
@@ -21,7 +38,8 @@ public class ProductDaoDB extends DB_connection implements ProductDao {
 
     @Override
     public void remove(int id) {
-
+        String statement = String.format("DELETE FROM products WHERE id = %d", id);
+        super.executeStatement(statement);
     }
 
     @Override
