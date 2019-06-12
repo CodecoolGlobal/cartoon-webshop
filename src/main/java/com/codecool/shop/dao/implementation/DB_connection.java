4c53d312714331408a3a2282345ff3168ee65bdc
Lizi Boros <boroslizi@gmail.com>
2019-06-12 09:18:49 +0200

    DB_connection added back to repo

---
+++ src/main/java/com/codecool/shop/dao/implementation/DB_connection.java
@@ -0,0 +1,32 @@
+package com.codecool.shop.dao.implementation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public abstract class DB_connection {
+
+    /*----------------You need to set your db account details here for proper application running---------------------*/
+
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/cartoon_shop";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "queenofsql";
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+
+    public void executeStatement(String SQL_Statement) {
+        try(Connection connection = getConnection();
+            PreparedStatement pStatement =connection.prepareStatement(SQL_Statement)) {
+                        pStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
