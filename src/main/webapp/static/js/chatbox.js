662442b9892c639475769454596574664f379d72
TheRandomHero <deadmaus840203>
2019-06-27 10:45:38 +0200

    live chat added

---
+++ src/main/webapp/static/js/chatbox.js
@@ -0,0 +1,131 @@
+
+
+(function($) {
+    $(document).ready(function() {
+        var $chatbox = $('.chatbox'),
+            $chatboxTitle = $('.chatbox__title'),
+            $chatboxTitleClose = $('.chatbox__title__close'),
+            $chatboxCredentials = $('.chatbox__credentials');
+        $chatboxTitle.on('click', function() {
+            $chatbox.toggleClass('chatbox--tray');
+        });
+        $chatboxTitleClose.on('click', function(e) {
+            e.stopPropagation();
+            $chatbox.addClass('chatbox--closed');
+        });
+        $chatbox.on('transitionend', function() {
+            if ($chatbox.hasClass('chatbox--closed')) $chatbox.remove();
+        });
+
+    });
+})(jQuery);
+
+let input = document.getElementById('btn-input');
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener('keyup', function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById('send-message').click();
+    }
+});
+
+document.getElementById('send-message').addEventListener('click', conversation);
+
+let messages = [];
+
+
+function sendMessage() {
+    let message = input.value;
+
+    let clientMessage = {
+        text: message,
+        author: 'client'
+    }
+    messages.push(clientMessage);
+    buildCloud();
+    document.getElementById('btn-input').value = "";
+}
+
+function getMessage() {
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(function (res) {
+            return res.json()
+                .then(function (data) {
+                    let serverResponse = {
+                        text: data.value,
+                        author: 'Chuck Norris'
+                    }
+                    messages.push(serverResponse)
+                }).then(function () {
+                    buildCloud();
+
+
+                })
+        })
+}
+
+function conversation() {
+    sendMessage();
+    getMessage();
+};
+
+
+
+
+function chatBoxClouds() {
+    let element =``;
+
+    for (let chat of messages){
+        element += `<div class ='chatbox__body__message chatbox__body__message--${chat['author']}'>
+               <div class="clearfix"></div>
+               <div class="ul_section_full">
+                     <ul class="ul_msg">
+                         <li><strong>${chat['author']}</strong></li>
+                         <li>${chat['text']}</li>
+                     </ul>
+               </div>
+           </div>`
+    }
+    return element;
+
+}
+
+
+function buildCloud() {
+        let chatBox = document.querySelector('.chatbox__body');
+
+    chatBox.innerHTML = chatBoxClouds();
+
+}
+// function createChatCloud(whoIsSpeaking, message) {
+//     let chatbody = document.querySelector('.chatbox__body')
+//     let div = document.createElement('div')
+//
+//
+//     chatbody.innerHTML =
+//
+//          `<div class ='chatbox__body__message chatbox__body__message--${whoIsSpeaking}'>
+//               <div class="clearfix"></div>
+//               <div class="ul_section_full">
+//                     <ul class="ul_msg">
+//                         <li><strong>${whoIsSpeaking}</strong></li>
+//                         <li>${message}</li>
+//                     </ul>
+//               </div>
+//           </div>  `;
+
+   // chatBox.innerHTML = `<div class ='chatbox__body__message chatbox__body__message--${whoIsSpeaking}'>
+   //             <div class="clearfix"></div>
+   //             <div class="ul_section_full">
+   //                   <ul class="ul_msg">
+   //                       <li><strong>${whoIsSpeaking}</strong></li>
+   //                       <li>${message}</li>
+   //                   </ul>
+   //             </div>
+   //         </div>  `;
+// }
+// }
